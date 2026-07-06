6c174069e418bfbdf5c8e850608184fa8199ef7f
briskt <3172830+briskt@users.noreply.github.com>
2026-07-06 14:01:11 -0600

    specify aws provider version in test

--- test/providers.tf
+++ test/providers.tf
@@ -2,7 +2,8 @@ terraform {
   required_version = ">= 1.0"
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      version = "~> 6.53"
+      source  = "hashicorp/aws"
     }
   }
 }
